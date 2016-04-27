abf1a544e7161e017911c84c1a1c5cd638d35b4d
Alfredo Martinez <alfredo.martinez.corona@gmail.com>
2016-04-27 12:18:07 -0700

    Title Screen to Game Flow.
    Title screen now loads the game scene. Added both the title screen and
    the game scene "Level1" to the "Scenes in Build" list.

---
+++ Assets/Scripts/LoadLevelOnAnyKey.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOnAnyKey : MonoBehaviour {
+
+	public string levelName;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown){
+			SceneManager.LoadScene (levelName);
+		}
+	}
+}
