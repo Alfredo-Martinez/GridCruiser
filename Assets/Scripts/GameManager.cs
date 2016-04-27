f0f9efb6adc8d8dc3f395d603985c9d1aeb2e423
Alfredo Martinez <alfredo.martinez.corona@gmail.com>
2016-04-27 11:17:08 -0700

    Score system and unified game speed control added.
    Score and high score kept independently and compared, used "gameSpeed"
    variable inside GameManager script for other scripts to reference.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,36 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement; //Neede to use SceneManager Class and it's functions
 
 public class GameManager : MonoBehaviour
 {
-	private bool gameOver = false; 
+	public static GameManager Instance; //Static so that there is only one instance at a time.
+	public float pointsPerUnitTravelled = 1.0f;
+	public float gameSpeed = 10.0f;
 
-	// Use this for initialization
-	void Start ()
-	{
-	
+	private bool gameOver = false;
+	private float score = 0.0f;
+	private static float highScore = 0.0f;
+
+	void Start () {
+		Instance = this;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (GameObject.FindGameObjectWithTag ("Player") == null)
-		{
+
+	void Update () {
+		if (GameObject.FindGameObjectWithTag ("Player") == null) {
 			gameOver = true;
 		}
 
-		if (gameOver)
-		{
+		if (gameOver) {
 			if (Input.anyKeyDown) {
-				Application.LoadLevel (Application.loadedLevel);
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			}
+		} 
+		else {
+			score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+			if (score > highScore) {
+				highScore = score;
 			}
 		}
 	}
 
-	void OnGUI()
-	{
-		if (gameOver)
-		{
+	void OnGUI() {
+
+		//Casts the float score to a an int then to a string.
+		GUILayout.Label ( "Score: " + ((int)(score)).ToString());
+		GUILayout.Label ( "HighScore: " + ((int)(highScore)).ToString());
+
+		if (gameOver) {
 			GUILayout.Label ("Game Over! Press any key to reset!");
 		}
 	}
---
+++ Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBasedOnGameSpeed: MonoBehaviour {
+
+	public Vector3 direction = Vector3.forward; //Using Z axis is less costly! Strange but true!
+
+
+	/*
+	void Start ()
+	{
+		
+	}
+	*/
+
+	void Update ()
+	{
+		//Check to see what normalized means.
+		transform.position += transform.rotation * (direction.normalized * GameManager.Instance.gameSpeed * Time.deltaTime);
+	}
+}
